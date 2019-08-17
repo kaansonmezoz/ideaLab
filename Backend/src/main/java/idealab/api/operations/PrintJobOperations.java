a164b33791e905df52398ecc4c1ebf4bb7f235b7
will-estes <will_estes@yahoo.com>
2019-08-16 21:26:59 -0600

    pushing via command line

--- Backend/src/main/java/idealab/api/operations/PrintJobOperations.java
+++ Backend/src/main/java/idealab/api/operations/PrintJobOperations.java
@@ -2,28 +2,48 @@ package idealab.api.operations;
 
 import idealab.api.dto.GenericResponse;
 import idealab.api.dto.PrintJobUpdateRequest;
+import idealab.api.models.EmployeeList;
+import idealab.api.models.PrintStatus;
+import idealab.api.repositories.EmployeeListRepo;
+import idealab.api.repositories.PrintStatusRepo;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrintJobOperations {
 
+    EmployeeListRepo employeeListRepo;
+    PrintStatusRepo printStatusRepo;
+
+    public PrintJobOperations(EmployeeListRepo employeeListRepo, PrintStatusRepo printStatusRepo) {
+        this.employeeListRepo = employeeListRepo;
+        this.printStatusRepo = printStatusRepo;
+    }
+
     public GenericResponse updatePrintJob(PrintJobUpdateRequest dto)
     {
         GenericResponse response = new GenericResponse();
+        response.setSuccess(false);
+        response.setMessage("Invalid Status");
 
         if(dto.isValidStatus())
         {
             //check if employee id is valid
+            EmployeeList employeeList = employeeListRepo.getEmployeeListById(dto.getEmployeeId());
+
             //check if print id is valid
-            //do any other logic to determine if the update is valid
-            response.setSuccess(true);
-            response.setMessage("Print Job Updated");
+            PrintStatus printStatus = printStatusRepo.getPrintStatusById(dto.getPrintStatusId());
+
+            if(employeeList != null && printStatus != null) {
+                //Update print status
+
+
+                //return success message
+                response.setSuccess(true);
+                response.setMessage("Print Job Updated");
+            }
+
         }
-        else
-        {
-            response.setSuccess(false);
-            response.setMessage("Invalid Status");
-        }
+
         return response;
     }
 
